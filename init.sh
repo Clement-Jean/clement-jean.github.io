b006f2f3aff0d5f2aac22fe9656fc97ee61fb8cf
Clement <clement.jean@epitech.eu>
2022-06-06 16:02:12 +0800

    disqus and analytics

--- init.sh
+++ init.sh
@@ -2,6 +2,6 @@ bundle init
 bundle add jekyll --version "~>4.2"
 bundle config set --local path "vendor/bundle"
 bundle install
-bundle add webrick jekyll-sitemap jekyll-feed jdvp-codetabs-commonmark
+bundle add webrick
 bundle install
 bundle update
