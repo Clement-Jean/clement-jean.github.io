65aea2780e18d3599e22e6d1591fd7cc72a472f6
Clement <clement.jean@epitech.eu>
2022-10-02 22:11:12 +0800

    better dependencies management

--- init.sh
+++ init.sh
@@ -1,7 +1,9 @@
 bundle init
-bundle add jekyll --version "~>4.2"
 bundle config set --local path "vendor/bundle"
-bundle install
-bundle add webrick
+bundle add jekyll --version "~>4.2"
+bundle add webrick --version "~> 1.7"
+bundle add jekyll-feed --version "~> 0.16.0"
+bundle add jekyll-sitemap --version "~> 1.4"
+bundle add jdvp-codetabs-commonmark --version "~> 0.1.2"
 bundle install
 bundle update
