1726a9859aebef14c7712a044307c79c5b05703c
Clement <clement.jean@epitech.eu>
2022-12-07 06:53:05 +0800

    update

--- init.sh
+++ init.sh
@@ -1,9 +1,7 @@
 bundle init
-bundle config set --local path "vendor/bundle"
 bundle add jekyll --version "~>4.2"
-bundle add webrick --version "~> 1.7"
-bundle add jekyll-feed --version "~> 0.16.0"
-bundle add jekyll-sitemap --version "~> 1.4"
-bundle add jdvp-codetabs-commonmark --version "~> 0.1.2"
+bundle config set --local path "vendor/bundle"
+bundle install
+bundle add webrick
 bundle install
 bundle update
