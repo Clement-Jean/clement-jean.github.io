1c28f277d070525d13029e62e461eefea98e6768
Clement <clement.jean@epitech.eu>
2023-01-08 17:57:44 +0800

    purgecss

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 JEKYLL_ENV=production bundle exec jekyll build
+purgecss --css _site/style.css --content _site/**/*.html --output _site
 git checkout master
 rsync -a -v _site/ ./
 git checkout working
