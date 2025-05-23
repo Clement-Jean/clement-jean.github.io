3ca7eb3dd6d378a970bcf53fedc1e4ffc558c158
Clement <clement.jean@epitech.eu>
2025-05-23 17:42:46 +0800

    purgecss for all css files

--- build.sh
+++ build.sh
@@ -3,6 +3,6 @@ set -o pipefail
 JEKYLL_ENV=production bundle exec jekyll build
 /Applications/Emacs.app/Contents/MacOS/Emacs -nw -Q --script build-site.el
 git checkout master
-purgecss --css _site/style.css --content _site/**/*.html --output _site
+purgecss --css _site/**/*.css --content _site/**/*.html --output _site
 rsync --exclude src -a -v _site/ ./
 git checkout working
