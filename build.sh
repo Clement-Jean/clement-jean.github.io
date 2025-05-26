0f5615c0073ceb2aa43c85b1865170b3b1220bc9
Clement <clement.jean@epitech.eu>
2025-05-26 17:09:08 +0800

    dont purge other css

--- build.sh
+++ build.sh
@@ -3,5 +3,5 @@ set -o pipefail
 JEKYLL_ENV=production bundle exec jekyll build
 /Applications/Emacs.app/Contents/MacOS/Emacs -nw -Q --script build-site.el
 git checkout master
-purgecss --css _site/css/*.css --content _site/**/*.html --output _site/css
+purgecss --css _site/css/style.css --content _site/**/*.html --output _site/css
 rsync --exclude src -a -v _site/ ./
